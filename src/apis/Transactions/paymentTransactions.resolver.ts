c601c28783fb3ec0fcab7c3ddc6ce841c4c60a51
iamdevray <lovotika@gmail.com>
2022-05-17 11:06:54 +0900

    Feat: [#7] 트랜잭션 오류 수정

--- src/apis/Transactions/paymentTransactions.resolver.ts
+++ src/apis/Transactions/paymentTransactions.resolver.ts
@@ -2,15 +2,14 @@ import { UseGuards } from "@nestjs/common";
 import { Args, Mutation, Resolver } from "@nestjs/graphql";
 import { GqlAuthAccessGuard } from "src/commons/auth/gql-auth.guard";
 import { CurrentUser, ICurrentUser } from "src/commons/auth/gql-user.param";
-import { Transaction } from "typeorm";
-import { IamportService } from "../iamport/iamport.service";
+import { PaymentTransaction } from "./entities/paymentTransaction.entity";
 import { PaymentTransactionService } from "./paymentTransaction.service";
 
 @Resolver()
 export class PaymentTransactionResolver {
     constructor(
         private readonly paymentTransactionService: PaymentTransactionService,
-        private readonly iamportService: IamportService,
+        // private readonly iamportService: IamportService,
     ) { }
 
     // @UseGuards(GqlAuthAccessGuard)
@@ -21,8 +20,8 @@ export class PaymentTransactionResolver {
 
 
     @UseGuards(GqlAuthAccessGuard)
-    @Mutation(() => Transaction)
-    createPointTransaction(
+    @Mutation(() => PaymentTransaction)
+    createPaymentTransaction(
         @Args('impUid') impUid: string,
         @Args('amount') amount: number,
         @CurrentUser() currentUser: ICurrentUser,
