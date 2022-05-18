9fa8e1fc7d80c9089e870adc21b775c7546d8ddd
iamdevray <lovotika@gmail.com>
2022-05-18 17:07:55 +0900

    Feat: [#16] 결제 고도화 작업 중(배포 준비를 위해 우선 PR 진행)

--- src/commons/auth/jwt-social-kakao.strategy.ts
+++ src/commons/auth/jwt-social-kakao.strategy.ts
@@ -6,22 +6,23 @@ import { Strategy } from "passport-kakao";
 export class JwtKakaoStrategy extends PassportStrategy(Strategy, "kakao") {
     constructor() {
         super({
-        clientID: process.env.OAUTH_KAKAO_ID,
-        clientSecret: process.env.OAUTH_KAKAO_SECRET,
-        callbackURL: process.env.OAUTH_KAKAO_CALLBACK,
-        scope: ['account_email', 'profile_nickname'],
-    })
-}
+            clientID: process.env.OAUTH_KAKAO_ID,
+            clientSecret: process.env.OAUTH_KAKAO_SECRET,
+            callbackURL: process.env.OAUTH_KAKAO_CALLBACK,
+            scope: ['account_email', 'profile_nickname'],
+        })
+    }
 
 
-validate(accessToken: string, refreshToken: string, profile: any) {
-    return {
-        email: !profile._json.kakao_account.email
-        ? "muzi@kakao.com"
-        : profile._json.kakao_account.email,
-        password: "qwer1234",
-        name: !profile.displayName ? "홍길동" : profile.displayName,
-        phone: !profile.mobile ? "010-5678-1234" : profile.mobile,
-    }}
+    validate(accessToken: string, refreshToken: string, profile: any) {
+        return {
+            email: !profile._json.kakao_account.email
+                ? "muzi@kakao.com"
+                : profile._json.kakao_account.email,
+            password: "qwer1234",
+            name: !profile.displayName ? "홍길동" : profile.displayName,
+            phone: !profile.mobile ? "010-5678-1234" : profile.mobile,
+        }
+    }
 }
 
