4ad9a08d722cd2d1a5724db2991d7547c0a058c2
iamdevray <lovotika@gmail.com>
2022-05-20 17:11:46 +0900

    Feat: [#16] 결제 고도화 - 데이터 트랜잭션 오류 해결을 위해 브랜치 변경

--- src/apis/auth/auth.service.ts
+++ src/apis/auth/auth.service.ts
@@ -13,14 +13,14 @@ export class AuthService {
     getAccessToken({ user }) {
         return this.jwtService.sign(
             { email: user.email, sub: user.user_id },
-            { secret: process.env.ACCESS_TOKEN, expiresIn: '1h' })
+            { secret: process.env.ACCESS_TOKEN, expiresIn: '1h' });
     }
 
     setRefreshToken({ user, res }) {
         const refreshToken = this.jwtService.sign(
             { email: user.email, sub: user.user_id },
-            { secret: process.env.REFRESH_TOKEN, expiresIn: '2w' })
-        res.setHeader('Set-Cookie', `refreshToken=${refreshToken}; path=/;`)
+            { secret: process.env.REFRESH_TOKEN, expiresIn: '2w' });
+        res.setHeader('Set-Cookie', `refreshToken=${refreshToken}; path=/;`);
 
         // 배포환경
         // res.setHeader('Access-Control-Allow-Origin', 'url')
@@ -34,20 +34,20 @@ export class AuthService {
     async socialLogin({ req, res }) {
         let user = await this.userService.findOne({
             email: req.user.email,
-        })
+        });
         if (!user) {
             user = await this.userService.createSocial({
                 email: req.user.email,
                 hashedPassword: req.user.password,
                 name: req.user.name,
                 phone: req.user.phone,
-            })
-        }
+            });
+        };
         this.setRefreshToken({ user, res })
         res.redirect(
             302,
             "http://localhost:5501/frontend/login/index.html",
-        )
-    };
+        );
+    }
 }
 
