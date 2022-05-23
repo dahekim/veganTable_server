33524941e9a5f4bb520137c09b9aa7004a93b563
dahekim <kimdre88@naver.com>
2022-05-23 14:46:38 +0900

    Fix: 회원가입 CORS 에러2

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,11 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter())
   app.use(graphqlUploadExpress())
   app.enableCors({
-    // origin: true,
     origin: 'http://localhost:3000',
+    credentials: true,
+    
     // frontend 배포 되면
     // origin: 'https://domain-url',
-    credentials: true,
   })
 
   await app.listen(3000)
