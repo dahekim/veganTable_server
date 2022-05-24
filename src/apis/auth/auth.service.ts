3847980a0ff0bab10aeddf9ab04561cc1e93ada3
dahekim <kimdre88@naver.com>
2022-05-24 15:09:39 +0900

    Error: [#56]로그인 에러

--- src/apis/auth/auth.service.ts
+++ src/apis/auth/auth.service.ts
@@ -26,15 +26,15 @@ export class AuthService {
 
         // 배포환경
         res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/')
-        res.setHeader(
-            'Set-Cookie',
-            `refreshToken=${refreshToken}; path=/; domain=.itoutsider.shop; SameSite=None; Secure; httpOnly;`,
-            )
         res.setHeader('Access-Control-Allow-Credentials', 'true');
         res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
         res.setHeader(
             'Access-Control-Allow-Headers',
             'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
+            )
+        res.setHeader(
+            'Set-Cookie',
+            `refreshToken=${refreshToken}; path=/; domain=.itoutsider.shop; SameSite=None; Secure; httpOnly;`,
             )
         }
 
